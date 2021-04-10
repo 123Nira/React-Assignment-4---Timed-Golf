6ddace9d7d301ef83312a734f2e7fddc8d025edd
123Nira <77059367+123Nira@users.noreply.github.com>
2021-04-10 16:00:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,101 +1,124 @@
-import React, { Component, useState } from "react";
+  
+import React, { Component, createRef, useState } from "react";
 import "../styles/App.css";
 class Timer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      time: 0, 
-      x: 0, 
-      y: 0,
-      renderBall: false,
-    };
-    this.handleClick = this.handleClick.bind(this);
-    this.timmer = this.timmer.bind(this);
-
+    this.state = { time: 0, x: 0, y: 0,clk:false,win:false , tm:createRef()};
+    this.handleclick = this.handleclick.bind(this);
+    this.handlekeypress = this.handlekeypress.bind(this);
+    
+   
   }
-
+  
 
   componentDidMount() {
-        window.addEventListener('keydown', (e) => {
-          switch (e.key) {
-              case 'ArrowRight':
-                this.setState({
-                    x: this.state.x + 5,
-                    y: this.state.y + 0,
-                })
-                break;
-
-              case 'ArrowDown':
-                this.setState({
-                  x: this.state.x + 0,
-                  y: this.state.y + 5,
-                })
-                
-                break;
-
-              case 'ArrowLeft':
-                this.setState({
-                  x: this.state.x - 5,
-                  y: this.state.y + 0,
-                })
-                break;
-
-              case 'ArrowUp':
-                this.setState({
-                  x: this.state.x + 0,
-                  y: this.state.y - 5,
-                })
-                break;
-          }
-      });
-
+    
       
-      if(this.state.x == 250 && this.state.y == 250){
-        console.log("I AM FROM IF");
-        window.clearInterval(interval);
-      }
-
+      //console.log("mount");
+    
+    
   }
+  
 
   componentWillUnmount() {
-    this.setState({
-      time : 0,
-      // renderBall: false,
-    })
+   // document.removeEventListener("keydown" ,this.handlekeypress)
+    
+   // console.log("componentunmount");
+  }
+  
+  
+  
+  
+
+  render_ball_and_hole(){
+    
+   if(this.state.clk)
+   {
+      return(
+        <>
+        
+        <div className="hole"></div>
+        </>
+      )
+   }
+   else{
+    return  <button className="start" onClick={this.handleclick} >start</button>
+   }
+
   }
 
-  timmer(){
+  handleclick(){
+    this.setState({clk:true})
+    document.addEventListener("keydown" , this.handlekeypress)
+    
 
-    if(this.state.x!=250 && this.state.y!=250){
-      this.setState({
-        time: this.state.time + 1,
-      })
-    }
-    else{
-      
-    }
+
+       var t =  setInterval(()=>{this.setState({time : this.state.time+1})} , 1000);
+        
+        this.setState({tm:t});
+    
+
+     
+  }
+
+  foo()
+  {
+    if(this.state.x == 250 && this.state.y == 250){
+         clearInterval(this.state.tm)
+         document.removeEventListener("keydown" ,this.handlekeypress)
+        
+       }
+       
+
+    // let sec = this.state.time % 60;
+    // let min = Math.floor(this.state.time / 60);
+    // let hour =Math.floor(this.state.time / 3600);
+
+    //  console.log(this.state.tm)
+    // return `${hour}:${min}:${sec}`
+  let sec = this.state.time
+  return `${sec}`
+    
   }
 
 
-  handleClick(){
-    // console.log("Start Button Clicked");
-    var interval = window.setInterval(this.timmer,1000);
+  //hadling keyboard events
+  handlekeypress(e){
+    //console.log(e.keyCode);
+    switch(e.keyCode)
+      { 
+        case 37:
+          this.setState({y:this.state.y -5})
+          //console.log(this.state);
+          break;
+        case 38:
+          this.setState({x:this.state.x -5})
+          //console.log(this.state);
+          break;
+        case 39:
+          this.setState({y: this.state.y + 5})
+          //console.log(this.state);
+          break;
+        case 40:
+          this.setState({x:this.state.x+5})
+          //console.log(this.state);
+          break;
+        
 
-    this.setState({
-      renderBall : true,
-    })
+      }
   }
 
 
   render() {
     return (
-      <>
-        <div className="ball" style={{left : this.state.x + "px" , top : this.state.y + "px"}}></div>
-        <div className="hole"></div>
-        <h1 className="heading-timer">{this.state.time}</h1>
-        <button className="start" onClick={this.handleClick}>Start</button>
-
-      </>
+        <>
+          <div className="ball" style={{top:this.state.x,left :this.state.y}}></div>
+          
+          {this.render_ball_and_hole()}
+          <div className="heading-timer">{ this.foo()}</div>
+ 
+        </>
     );
   }
 }
